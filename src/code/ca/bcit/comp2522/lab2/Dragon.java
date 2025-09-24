ed40d29e3b0e9168ad0a5a5ed89ee28094758e70
samuelpitabcit <231584167+samuelpitabcit@users.noreply.github.com>
2025-09-24 08:47:15 -0700

    Added LowFirePowerException

--- src/code/ca/bcit/comp2522/lab2/Dragon.java
+++ src/code/ca/bcit/comp2522/lab2/Dragon.java
@@ -2,7 +2,16 @@ package ca.bcit.comp2522.lab2;
 
 import java.time.LocalDate;
 
+/**
+ * @author Samuel Pita
+ * @version 1.0
+ */
 public class Dragon extends Creature {
+
+    private static final int MIN_FIRE_POWER = 10;
+    private static final int MAX_FIRE_POWER = 100;
+
+    private int firePower;
 
     /**
      * Constructs a new Creature instance.
@@ -11,7 +20,18 @@ public class Dragon extends Creature {
      * @param dateOfBirth The date of birth of the creature.
      * @param health      The initial health of the creature.
      */
-    public Dragon(final String name, final LocalDate dateOfBirth, final int health) {
+    public Dragon(final String name,
+                  final LocalDate dateOfBirth,
+                  final int health,
+                  final int firePower) {
         super(name, dateOfBirth, health);
+
+        validateFirePower(firePower);
+        this.firePower = firePower;
+    }
+
+    private static void validateFirePower(final int firePower) {
+        if (firePower < MIN_FIRE_POWER || firePower > MAX_FIRE_POWER)
+            throw new IllegalArgumentException("Fire power must be set between " + MIN_FIRE_POWER + " and " + MAX_FIRE_POWER);
     }
 }
---
+++ src/code/ca/bcit/comp2522/lab2/LowFirePowerException.java
@@ -0,0 +1,7 @@
+package ca.bcit.comp2522.lab2;
+
+public class LowFirePowerException extends RuntimeException {
+    public LowFirePowerException(String message) {
+        super(message);
+    }
+}
